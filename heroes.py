6bcc2544c9c964bcd107e4b7d3bb53b7822cec3b
Pavel Repin <sghcd2012@yandex.ru>
2022-09-23 17:41:51 +0300

    file: python.py add if __name__ == '__main__':

--- heroes.py
+++ heroes.py
@@ -1,10 +1,8 @@
 import requests
 import pprint
-import json
 
 heroes_dict = {}
 def halk():
-
     hero_url = requests.get("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/332.json")
     data_name = hero_url.json()
     # pprint(data_name)
@@ -14,44 +12,47 @@ def halk():
     data_powerstats = heroe_powerstats.json()
     powerstats = data_powerstats["intelligence"]
     heroes_dict[name] = powerstats
-    # print(f'{name} {powerstats}')
-halk()
+
+
 
 
 def cap():
     hero_url = requests.get("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/149.json")
     data_name = hero_url.json()
-    # pprint(data_name)
     name = data_name['name']
 
     heroe_powerstats = requests.get('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/powerstats/149.json')
     data_powerstats = heroe_powerstats.json()
     powerstats = data_powerstats["intelligence"]
     heroes_dict[name] = powerstats
-    # print(f'{name} {powerstats}')
 
-cap()
+
 
 def  thanos():
     hero_url = requests.get("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/655.json")
     data_name = hero_url.json()
-    # pprint(data_name)
     name = data_name['name']
 
     heroe_powerstats = requests.get('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/powerstats/655.json')
     data_powerstats = heroe_powerstats.json()
     powerstats = data_powerstats["intelligence"]
     heroes_dict[name] = powerstats
-    # print(f'{name} {powerstats}')
 
-thanos()
 
 def max_intelligence():
     heroes_val = heroes_dict.items()
     heroes_val_lst = list(heroes_val)
     kv = max(heroes_val_lst, key=lambda i: i[1])
-    # print(kv)
     name = kv[0]
     intelligence = kv[1]
-    print(f'{name} - {intelligence}')
-max_intelligence()
+    print(f'Самый умный герой {name} его интелект равен: {intelligence}.')
+
+
+def main():
+    halk()
+    cap()
+    thanos()
+    max_intelligence()
+
+if __name__ == '__main__':
+    main()
